46d024a818f01799229e5f1d7c299a6874fc59f1
Tong Wu <wutong0910@foxmail.com>
2022-01-22 11:51:17 +0800

    learnku-hub

--- learnku-hub/bootstrap/database.go
+++ learnku-hub/bootstrap/database.go
@@ -3,7 +3,6 @@ package bootstrap
 import (
 	"errors"
 	"fmt"
-	"gohub/app/models/user"
 	"gohub/pkg/config"
 	"gohub/pkg/database"
 	"gohub/pkg/logger"
@@ -50,9 +49,4 @@ func SetupDB() {
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
-
-	e := database.DB.AutoMigrate(&user.User{})
-	if e != nil {
-		panic(e)
-	}
 }
